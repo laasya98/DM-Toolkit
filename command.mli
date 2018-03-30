1f2879870d22e5bcf91128b1b514177710c26738
Allan Buchness <vagrant@vm3110-2018sp>
2018-03-29 22:48:42 -0400

    added to the command variant and included parse

--- command.mli
+++ command.mli
@@ -4,6 +4,33 @@
     begin changing according to the client's actions.*)
 
 module type Command = sig
+
+  (* command is an imcomplete variant of command possibilities, which will be
+     updated as more functionality is needed.  *)
   type command =
-    |
+    |Load of string
+    |Save
+    |Quit
+    |Help
+    |Event of string
+    |Inquiry
+    |Move of (string *string)
+    |Use of (string * string)
+    |Inventory of string
+    |Give of string
+    |Take of string
+    |Drop of string
+    |Shop of string
+    |Fight of string list
+    |CreateEntity
+    |CharacterAction
+    |StateChange of (string * string)
+    |ItemChange of (string * string)
+    |Roll of (string * string)
+    |Invalid
+
+  (** [parse str] is the command parsed from the player input [str].
+      [parse] requires any string, which will be parsed into one of the commands
+      above, defaulting to Invalid for any non-specific string.*)
+  val parse : string -> command
 end
