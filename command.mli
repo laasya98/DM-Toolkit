b8e45fd8a8c06a589bfabc914d58b9f541636cc9
Allan Buchness <vagrant@vm3110-2018sp>
2018-03-29 21:53:05 -0400

    added command and started work on state declarations

---
+++ command.mli
@@ -0,0 +1,9 @@
+
+(** A [Command] is a player input that calls an event, retrieves information,
+    manipulates the state or characer, or otherwise causes the gamespace to
+    begin changing according to the client's actions.*)
+
+module type Command = sig
+  type command =
+    |
+end
--- state.mli
+++ state.mli
@@ -1,4 +1,26 @@
+open Database
+open Event
+open Character
+open Command
+open Item
 
+(** A [State] is a representation of the state of the toolkit, including every
+    current location and entity, and any statuses or active effects for the
+    current gamespace.*)
 module type State = sig
+  module D : Database
+  module C : Character
+  module I : Item
+
+(* [data] is the type of data in database.mli *)
+  type data = D.data
+
+(* [character] is the type of a character in character.mli *)
+  type character = C.c
+
+(* [item] is the type of an item in item.mli *)
+  type item = I.i
+
+(* [t] is the type of a state*)
   type t
 end
