65508912440a66ff278c27d2e0512a9edabf4c03
FeatheredScorpion <31936653+FeatheredScorpion@users.noreply.github.com>
2018-04-25 15:39:57 -0400

    Added item data type to Global

--- global.mli
+++ global.mli
@@ -1,1 +1,35 @@
-type item = string 
+
+(* For now, just change a stat by set amount. *)
+type effect = {
+  stat:string; (*TODO: temp. Coordinate with character?*)
+  amount:int;
+}
+
+type wtype = Ranged | Melee
+
+type weapontype = {
+  t : wtype;
+  damage : int;
+  dice : int list;
+}
+
+type armortype = {
+  ac : int
+}
+
+type itemtype =
+  | Weapon of weapontype
+  | Shield
+  | Armor of armortype
+  | Ring
+  | Other
+
+type item = {
+  name:string;
+  i_type:itemtype;
+  description:string;
+  weight:int;
+  effect:effect;
+  uses:int;
+  value:int;
+}
