f3031027b78d1d52d739a540081c84710f0daa80
lae73 <lae73@cornell.edu>
2018-05-14 19:17:45 -0400

    save

--- database.mli
+++ database.mli
@@ -12,7 +12,7 @@ module type Database = sig
   val load_data : string -> data
 
   (** [save_data f d] writes a data object to a file [f]  *)
-  val save_data : string -> data -> unit
+  val save_data : string -> string list list -> unit
 
   (** [hit_die class] is the hit die belonging to a given class
       represented by the string [class]
